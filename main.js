7972b51accb9bb616d0a82dd12250c197520f7e2
Masha0701 <reflection2008@gmail.com>
2024-03-28 16:38:32 +0300

    feat: orbit controls

--- main.js
+++ main.js
@@ -4,16 +4,17 @@ import WebGL from 'three/addons/capabilities/WebGL.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const axesHelper = new THREE.AxesHelper(3);
+// const renderer = new THREE.WebGLRenderer();
 
 
 const scene = new THREE.Scene();
 const canvas = document.querySelector('.canvas');
+const renderer = new THREE.WebGLRenderer({ canvas });
 
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-};
-
+}
 const cursor = {
   x: 0,
   y: 0,
@@ -29,9 +30,11 @@ const material = new THREE.MeshBasicMaterial({
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-scene.add(axesHelper)
+scene.add(axesHelper);
+
 camera.position.z = 3;
 const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true;
 
 window.addEventListener('mousemove', (event) => {
   cursor.x = -(event.clientX / sizes.width - 0.5);
@@ -39,10 +42,11 @@ window.addEventListener('mousemove', (event) => {
 });
 
 const tick = () => {
-  camera.position.x = Math.sin((cursor.x * Math.PI * 2 ) * 2);
-  camera.position.z = Math.cos((cursor.x * Math.PI * 2 ) * 2);
-  camera.position.y = cursor.y * 2;
-  camera.lookAt(cube.position)
+  // camera.position.x = Math.sin((cursor.x * Math.PI * 2 ) * 2);
+  // camera.position.z = Math.cos((cursor.x * Math.PI * 2 ) * 2);
+  // camera.position.y = cursor.y * 2;
+  // camera.lookAt(cube.position)
+  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 }
