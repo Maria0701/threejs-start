e7ac706a7d8caa9fb7a36afc3a6107990cbaa9c1
Masha0701 <reflection2008@gmail.com>
2024-03-28 13:14:13 +0300

    feat: cunes animations

--- main.js
+++ main.js
@@ -6,61 +6,77 @@ const axesHelper = new THREE.AxesHelper(3);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
 const scene = new THREE.Scene();
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
+  color: 'yellow',
   wireframe: true,
 });
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-scene.add(axesHelper)
-camera.position.z = 5;
-// camera.position.y = 1;
-// camera.lookAt(new THREE.Vector3(0,-1, 0))
-// cube.position.x = -1;
-// cube.position.y = -0.8;
-// cube.position.z = 0.5;
-// cube.scale.x = 0.5;
-// cube.scale.y = 2;
-// cube.scale.z = 0.7;
-// cube.rotation.x = Math.PI * 0.25;
-// cube.rotation.y = Math.PI *0.25;
-// function animate() {
-//   requestAnimationFrame(animate);
-//   cube.rotation.x += 0.01;
-//   cube.rotation.y += 0.01;
-//   renderer.render(scene, camera);
-// }
+// scene.add(cube);
+camera.position.z = 3;
 
-// const group = new THREE.Group();
-// group.scale.y = 1.4;
-// group.rotation.x = Math.PI * 0.25;
-// const cube1 = new THREE.Mesh(geometry, material);
-// cube1.position.x = -1.2;
-// const cube2 = new THREE.Mesh(geometry, material);
-// cube2.position.x = 0;
-// const cube3 = new THREE.Mesh(geometry, material);
-// cube3.position.x = 1.2;
-// group.add(cube1);
-// group.add(cube2);
-// group.add(cube3);
-// scene.add(group);
-const clock = new THREE.Clock();
-const tick = () => {
-  const elepsedTime = clock.getElapsedTime();
-  // cube.rotation.y = elepsedTime;
-  camera.position.x = Math.cos(elepsedTime);
-  camera.position.y = Math.sin(elepsedTime);
-  camera.lookAt(cube.position);
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(tick)
+const group = new THREE.Group();
+
+const mashes = [];
+const colors = ['pink', 'red', 'yellow', 'green', 'pink', 'blue'];
+
+for (let x = -1.2; x <= 1.2; x = x + 1.2) {
+  for (let y = - 1.2; y <= 1.2; y = y + 1.2) {
+    const material = new THREE.MeshBasicMaterial({
+      color: colors[((Math.random() * 5) | 0) + 1],
+      wireframe: true,
+    })
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set(0.5, 0.5, 0.5);
+    mesh.position.set(x, y, 0);
+    mashes.push(mesh);
+  }
 }
+
+group.add(...mashes);
+
+scene.add(group);
+scene.add(axesHelper);
+const clock = new THREE.Clock();
+
+const MAX_SCALE = 1;
+const MIN_SCALE = 0.5;
+let grow = false;
+
+const animate = () => {
+  const delta = clock.getDelta();
+
+  mashes.forEach((item, index) => {
+    const mult = index % 2 === 0 ? 1 : -1;
+    item.rotation.x += mult * delta;
+    item.rotation.y += mult * delta * 0.4;
+  });
+
+  const elapsed = clock.getElapsedTime();
+  camera.position.x = Math.sin(elapsed);
+  camera.position.y = Math.cos(elapsed);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  const mult = grow ? 1 : -1;
+  group.scale.x += mult * delta * 0.2;
+  group.scale.y += mult * delta * 0.2;
+  group.scale.z += mult * delta * 0.2;
+  if (grow && group.scale.x >= MAX_SCALE) {
+    grow = false;
+  } else if (!grow && group.scale.y <= MIN_SCALE) {
+    grow = true;
+  } 
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+}
+
+
 if (WebGL.isWebGLAvailable()) {
   // Initiate function or other initializations here
-  tick();
-
+  // renderer.render(scene, camera);
+  animate()
 } else {
 
   const warning = WebGL.getWebGLErrorMessage();
