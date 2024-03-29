2302d43357b5a2322b132551355a0eec0f7db5da
Masha0701 <reflection2008@gmail.com>
2024-03-29 10:16:48 +0300

    feat: wireframe added

--- main.js
+++ main.js
@@ -1,92 +1,78 @@
 import * as THREE from 'three';
+
+import init from './init';
+
 import './style.css';
-import WebGL from 'three/addons/capabilities/WebGL.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-const axesHelper = new THREE.AxesHelper(3);
-const scene = new THREE.Scene();
-const canvas = document.querySelector('.canvas');
-const renderer = new THREE.WebGLRenderer({ canvas });
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
+const { sizes, camera, scene, canvas, controls, renderer } = init();
+
+camera.position.z = 3;
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({
+// 	color: 'gray',
+// 	wireframe: true,
+// });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+const group = new THREE.Group();
+
+const geometries = [
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.ConeGeometry(1, 2, 32, 1),
+  new THREE.RingGeometry(0.5, 1, 16),
+  new THREE.TorusGeometry(1, 0.5, 16, 100),
+  new THREE.DodecahedronGeometry(1, 0),
+  new THREE.SphereGeometry(1, 32, 16),
+  new THREE.TorusKnotGeometry(1, 0.25, 100, 16, 1, 5),
+  new THREE.OctahedronGeometry(1, 0),
+  new THREE.CylinderGeometry(0.5, 1, 2, 16, 4),
+]
+let index = 0;
+for (let j = -5; j <= 5; j += 5) {
+  for (let i = -5; i <= 5; i += 5) {
+    const material = new THREE.MeshBasicMaterial({
+      color: 'gray',
+      wireframe: true,
+    });
+    
+    const mesh = new THREE.Mesh(geometries[index], material);
+    mesh.position.set(j, i, 10);
+    group.add(mesh);
+    index += 1;
+  }
 }
-renderer.setSize(sizes.width, sizes.height);
-// document.body.appendChild(renderer.domElement);
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 500);
-// const geometry = new THREE.BoxGeometry(1, 1, 1, 10, 10, 10);
-// const geometry =  new THREE.CircleGeometry(1,20, 0, Math.PI);
-// const geometry = new THREE.PlaneGeometry(1, 2, 10, 10);
-// const geometry = new THREE.ConeGeometry(1,2,32, 1, true, 0, Math.PI);
-// const geometry = new THREE.CylinderGeometry(0.5,1,2,32, 4, true, 0, Math.PI / 2);
-// const geometry = new THREE.RingGeometry(0.5, 1, 30, 10, 0, Math.PI / 2);
-// const geometry = new THREE.TorusGeometry(1, 0.5, 16, 100, Math.PI)
-// const geometry = new THREE.TorusKnotGeometry(1, 0.25, 100, 16, 1, 6)
-// const geometry = new THREE.DodecahedronGeometry(1, 0);
-// const geometry = new THREE.OctahedronGeometry(1, 0);
-// const geometry = new THREE.TetrahedronGeometry(1, 0);
-// const geometry = new THREE.SphereGeometry(1, 32, 16, 0, Math.PI * 2, 0, Math.PI / 2);
-const geometry = new THREE.BufferGeometry();
-const amount = 500;
-const points = new Float32Array(amount * 3 * 3);
-for ( let i = 0; i < amount *3 * 3; i++) {
-  points[i] = (Math.random() - 0.5) * 4;
-}
-const pointsBuffer = new THREE.BufferAttribute(points, 3);
-geometry.setAttribute('position', pointsBuffer)
 
+scene.add(group);
 
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,
-});
-
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true;
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-scene.add(axesHelper)
-camera.position.z = 5;
-
-const clock = new THREE.Clock();
 const tick = () => {
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
-}
-if (WebGL.isWebGLAvailable()) {
-  tick();
+};
+tick();
 
-} else {
+/** Базовые обпаботчики событий длы поддержки ресайза */
+window.addEventListener('resize', () => {
+  // Обновляем размеры
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-  const warning = WebGL.getWebGLErrorMessage();
-  document.getElementById('container').appendChild(warning);
+  // Обновляем соотношение сторон камеры
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-}
-
-const resizeObserver = new ResizeObserver((entries) => {
-  for (const entry of entries) {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-    console.log(sizes)
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    renderer.render(scene, camera);
-  }
-}
-);
-
-resizeObserver.observe(document.querySelector('body'), {
-  childList: true,
-  subtree: true,
+  // Обновляем renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.render(scene, camera);
 });
 
 window.addEventListener('dblclick', () => {
-  if (document.fullscreenElement) {
-    document.exitFullscreen();
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen();
   } else {
-    document.querySelector('canvas').requestFullscreen();
+    document.exitFullscreen();
   }
-})
+});
